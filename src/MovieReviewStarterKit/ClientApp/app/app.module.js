c79c672ebd9aeda4b301dbe0fd36904ddf322de5
Rahul Sahay <rahulsahay19@gmail.com>
2016-12-12 23:40:41 +0530

    Updated scripts

--- src/MovieReviewStarterKit/ClientApp/app/app.module.js
+++ src/MovieReviewStarterKit/ClientApp/app/app.module.js
@@ -15,7 +15,6 @@ var app_component_1 = require("./components/app/app.component");
 var navmenu_component_1 = require("./components/navmenu/navmenu.component");
 var home_component_1 = require("./components/home/home.component");
 var fetchdata_component_1 = require("./components/fetchdata/fetchdata.component");
-var counter_component_1 = require("./components/counter/counter.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -27,7 +26,6 @@ AppModule = __decorate([
         declarations: [
             app_component_1.AppComponent,
             navmenu_component_1.NavMenuComponent,
-            counter_component_1.CounterComponent,
             fetchdata_component_1.FetchDataComponent,
             home_component_1.HomeComponent
         ],
@@ -36,7 +34,6 @@ AppModule = __decorate([
             router_1.RouterModule.forRoot([
                 { path: '', redirectTo: 'home', pathMatch: 'full' },
                 { path: 'home', component: home_component_1.HomeComponent },
-                { path: 'counter', component: counter_component_1.CounterComponent },
                 { path: 'fetch-data', component: fetchdata_component_1.FetchDataComponent },
                 { path: '**', redirectTo: 'home' }
             ])
